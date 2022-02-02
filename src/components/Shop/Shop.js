132f23594ef11188cb52c40db013ef671c2b456c
Uchchash346 <mru416@gmail.com>
2022-02-02 18:25:52 +0600

    setting the cart on the localstorage for retrieve

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb } from '../../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -16,6 +17,8 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        // save to local storage (for now)
+        addToDb(product.key);
     }
 
     return (
