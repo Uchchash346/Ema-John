a5696d7bc36d787e8a888acc65e56da2db0d6525
Uchchash346 <mru416@gmail.com>
2022-02-02 18:44:15 +0600

    Find product

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css';
 
 const Shop = () => {
@@ -12,6 +12,15 @@ const Shop = () => {
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => setProducts(data))
+    }, [])
+
+    useEffect(() => {
+        const savedCart = getStoredCart();
+        for (const key in savedCart) {
+            console.log(key);
+            const addedProduct = products.find(product => product.key === key);
+            console.log(key, addedProduct);
+        }
     }, [])
 
     const handleAddToCart = (product) => {
